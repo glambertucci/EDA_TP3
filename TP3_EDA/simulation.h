b3a95b51b75e9f043e31720cefd8467c1ae087ac
Guido <gpanaggio@itba.edu.ar>
2018-03-17 23:23:21 -0300

    empece a hacer el simulacion.h

--- TP3_EDA/simulation.h
+++ TP3_EDA/simulation.h
@@ -4,6 +4,18 @@
 #include "robot.h"
 #include "floor.h"
 
+class SIMULATION
+{
+public:
+	bool create(unsigned int robots, unsigned int rows, unsigned int columns, unsigned int mode);
+	unsigned int run();		//devuelve los ticks
+	void destroy();
 
+private:
+	ROBOT robs[];
+	unsigned int ticks;
+	FLOOR *floor;
+	int mode;
+};
 
 #endif
